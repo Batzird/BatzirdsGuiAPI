8f4e4d28d60d10d06827d29f6b63e297a303ac20
Batzird <batzird@outlook.com>
2026-04-26 11:07:11 -0500

    Implement Listener interface in BatzirdsGuiAPI

--- src/main/java/com/batzirdbuilds/batzirdsGuiAPI/BatzirdsGuiAPI.java
+++ src/main/java/com/batzirdbuilds/batzirdsGuiAPI/BatzirdsGuiAPI.java
@@ -3,13 +3,15 @@ package com.batzirdbuilds.batzirdsGuiAPI;
 import com.batzirdbuilds.batzirdsGuiAPI.runtime.GuiListener;
 import com.batzirdbuilds.batzirdsGuiAPI.runtime.GuiSessionManager;
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Plugin bootstrap for the GUI API runtime services.
  */
-public final class BatzirdsGuiAPI extends JavaPlugin {
+public final class BatzirdsGuiAPI extends JavaPlugin, Listener {
 
     @Override
     public void onEnable() {
@@ -18,6 +20,6 @@ public final class BatzirdsGuiAPI extends JavaPlugin {
 
     @Override
     public void onDisable() {
-    
     }
+    
 }
