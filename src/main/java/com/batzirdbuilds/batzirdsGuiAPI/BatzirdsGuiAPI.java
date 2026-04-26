a836103fbbacfd76b9ac29751a3ca94cc94d21ac
Batzird <batzird@outlook.com>
2026-04-26 12:11:32 -0500

    Refactor BatzirdsGuiAPI by removing plugin methods
    Removed unnecessary imports and plugin lifecycle methods.

--- src/main/java/com/batzirdbuilds/batzirdsGuiAPI/BatzirdsGuiAPI.java
+++ src/main/java/com/batzirdbuilds/batzirdsGuiAPI/BatzirdsGuiAPI.java
@@ -1,29 +1,15 @@
 package com.batzirdbuilds.batzirdsGuiAPI;
 
-import com.batzirdbuilds.batzirdsGuiAPI.runtime.GuiListener;
-import com.batzirdbuilds.batzirdsGuiAPI.runtime.GuiSessionManager;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import com.batzirdbuilds.batzirdsGuiAPI.SilentConsoleSender;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Plugin bootstrap for the GUI API runtime services.
  */
-public final class BatzirdsGuiAPI extends JavaPlugin implements Listener {
-
-    @Override
-    public void onEnable() {
-        getLogger().info("BatzirdsGuiAPI is not meant to be run as a plugin. Please shade this API into your plugins as a dependency!");
-    }
-
-    @Override
-    public void onDisable() {
-    }
+public final class BatzirdsGuiAPI implements Listener {
 
     @EventHandler
     public void onBookEdit(PlayerEditBookEvent event) {
